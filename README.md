4aea6413a5b4e3c4178921f9c11d72b95010a8a3
Sebastien Deleuze <sdeleuze@pivotal.io>
2016-12-27 14:44:33 +0100

    Fix a typo

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ These templates look like:
 
 ```kotlin
 import io.spring.demo.User
-import io.spring.demo.jointToLine
+import io.spring.demo.joinToLine
 
 """
 ${include("header", bindings)}
 <h1>Title : $title</h1>
 <ul>
-	${(users as List<User>).jointToLine{ "<li>User ${it.firstname} ${it.lastname}</li>" }}
+	${(users as List<User>).joinToLine{ "<li>User ${it.firstname} ${it.lastname}</li>" }}
 </ul>
 ${include("footer")}
 """
--- src/main/resources/templates/index.kts
+++ src/main/resources/templates/index.kts
@@ -1,12 +1,12 @@
 import io.spring.demo.User
 import io.spring.demo.include
-import io.spring.demo.jointToLine
+import io.spring.demo.joinToLine
 
 // TODO Use foo instead of bindings["foo"] when https://youtrack.jetbrains.com/issue/KT-15125 will be fixed
 // TODO Discuss with Kotlin team how to avoid undefined bind variable reported as errors
 """${include("header", bindings)}
 <h1>Title : ${bindings["title"]}</h1>
 <ul>
-${(bindings["users"] as List<User>).jointToLine { "<li>User ${it.firstname} ${it.lastname}</li>" }}
+${(bindings["users"] as List<User>).joinToLine { "<li>User ${it.firstname} ${it.lastname}</li>" }}
 </ul>
 ${include("footer")}"""
