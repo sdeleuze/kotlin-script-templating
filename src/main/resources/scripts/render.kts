41a59786f31be54ecd3b510e496237e07b7b0108
Sebastien Deleuze <sdeleuze@pivotal.io>
2017-08-28 17:27:58 +0200

    Add basic cache implementation

--- src/main/resources/scripts/render.kts
+++ src/main/resources/scripts/render.kts
@@ -1,13 +1,20 @@
+import io.spring.demo.compilableEngine
 import org.springframework.web.servlet.view.script.RenderingContext
 import org.springframework.context.support.ResourceBundleMessageSource
 import javax.script.*
 import org.springframework.beans.factory.getBean
+import java.util.concurrent.ConcurrentHashMap
+
+
 
 fun render(template: String, model: Map<String, Any>, renderingContext: RenderingContext): String {
-	val engine = ScriptEngineManager().getEngineByName("kotlin")
+	var cache = renderingContext.applicationContext.getBean<ConcurrentHashMap<String, CompiledScript>>()
+	val compiledScript = cache.getOrPut(renderingContext.url,  { compilableEngine().compile(template);  })
 	val bindings = SimpleBindings(model)
-	var messageSource = renderingContext.applicationContext.getBean<ResourceBundleMessageSource>()
+	val messageSource = renderingContext.applicationContext.getBean<ResourceBundleMessageSource>()
 	bindings.put("i18n", { code: String -> messageSource.getMessage(code, null, renderingContext.locale) })
 	bindings.put("include", { path: String -> renderingContext.templateLoader.apply("templates/$path.kts") })
-	return engine.eval(template, bindings) as String
+	bindings.put("cache", cache)
+	return compiledScript.eval(bindings) as String
 }
+
